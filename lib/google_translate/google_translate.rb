3c8725aac28a57fe68d21c7d9a9c12622ddb0022
Sergey <forrest.ua@gmail.com>
2016-03-20 18:38:41 +0100

    fix function call

--- lib/google_translate/google_translate.rb
+++ lib/google_translate/google_translate.rb
@@ -79,8 +79,7 @@ class GoogleTranslate
   def call_service url, body
     accessor = ResourceAccessor.new
 
-    accessor.get_response {:url => url, :method => :post, :body => body}, 
-      {'User-Agent' => 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5'}
+    accessor.get_response {:url => url, :method => :post, :body => body}, {'User-Agent' => 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 4_3_3 like Mac OS X; en-us) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8J2 Safari/6533.18.5'}
   end
 
   def collect_languages buffer, index, tag_name, tag_id
