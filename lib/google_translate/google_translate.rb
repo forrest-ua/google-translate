67c6bd3a734a3a69c26e383329c011ce9bd85724
Sergey <forrest.ua@gmail.com>
2016-03-20 19:44:31 +0100

    update query

--- lib/google_translate/google_translate.rb
+++ lib/google_translate/google_translate.rb
@@ -63,7 +63,7 @@ class GoogleTranslate
   def call_translate_service from_lang, to_lang, text
     url = translate_url(from_lang, to_lang)
 
-    response = call_service url, "q=#{text}"
+    response = call_service "#{url}&tk=" + tl(text), "q=#{text}"
 
     response.body.split(',').collect { |s| s == '' ? "\"\"" : s }.join(",") # fix json object
   end
@@ -114,6 +114,86 @@ class GoogleTranslate
 
     matches.map { |m| Language.new(m[2], m[1]) }
   end
+
+
+  def shr32(x, bits)
+    return x if bits.to_i <= 0
+    return 0 if bits.to_i >= 32
+
+    bin = x.to_i.to_s(2) # to binary
+    l = bin.length
+    if l > 32
+      bin = bin[(l - 32), 32]
+    elsif l < 32
+      bin = bin.rjust(32, '0')
+    end
+
+    bin = bin[0, (32 - bits)]
+    (bin.rjust(32, '0')).to_i(2)
+  end
+
+  def char_code_at(str, index)
+    char = str[index]
+    v = char.unpack('H*')[0]
+    v.to_i(16)
+  end
+
+  def rl(a, b)
+    c = 0
+    while c < (b.length - 2) do
+      d = b[c+2]
+      d = (d >= 'a') ? char_code_at(d, 0) - 87 : d.to_i
+      d = (b[c+1] ==  '+') ? shr32(a, d) : a << d
+      a = (b[c] == '+') ? (a + d & 4294967295) : a ^ d
+      c += 3
+    end
+    a
+  end
+
+  def tl(a)
+    b = 402890
+    d = []
+    e = 0
+    f = 0
+
+    while f < a.length do
+      g = char_code_at(a, f)
+      if 128 > g
+        d[e] = g
+      else
+        if 1048 > g
+          d[e] = g >> 6 | 192
+        else
+          if (55296 == (g & 64512) && f + 1 < a.length && 56320 == (char_code_at(a, (f+1)) & 64512))
+            g = 65536 + ((g & 1023) << 10) + (char_code_at(a, ++f) & 1023)
+            d[e] = g >> 18 | 240
+            d[e] = g >> 12 & 63 | 128
+          else
+            d[e] = g >> 12 | 224
+            d[e] = g >> 6 & 63 | 128
+          end
+        end
+        d[e] = g & 63 | 128
+      end
+      e +=1
+      f += 1
+    end
+
+    a = b
+    e = 0
+
+    while e < d.length do
+      a += d[e]
+      a = rl(a, '+-a^+6')
+      e += 1
+    end
+
+    a = rl(a, "+-3^+b+-f")
+    a = (a & 2147483647) + 2147483648 if 0 > a
+    a %= 10 ** 6
+    return ("#{ a }.#{ a ^ b }")
+  end
+
 end
 
 
